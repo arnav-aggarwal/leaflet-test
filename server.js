234b98838726b02b89f512c6fbcd0d50227b8070
arnav-aggarwal <arnav.aggarwal@thecontrolgroup.com>
2018-04-05 15:03:54 -0700

    Clean up server.js

--- server.js
+++ server.js
@@ -2,14 +2,6 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use((req, res, next) => {
-	const { url } = req;
-	next();
-});
-
-// app.use(express.static(path.join(__dirname, 'leaflet')));
-app.get('/', (req, res) =>
-	res.sendFile(path.join(__dirname, 'index.html'))
-);
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 app.use(express.static(__dirname));
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
