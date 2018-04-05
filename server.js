e24d5706b4477c37026575d30bc0810dd7cab05b
arnav-aggarwal <arnav.aggarwal@thecontrolgroup.com>
2018-04-05 11:27:30 -0700

    Add leaflet files, remove CDN link

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname));
 // app.use(express.static(path.join(__dirname, 'leaflet')));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname,'index.html')));
+app.use(express.static(__dirname));
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
