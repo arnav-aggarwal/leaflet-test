43a44b7078971a3f104dcc7e88e981be4eb2976e
arnav-aggarwal <arnav.aggarwal@thecontrolgroup.com>
2018-04-09 15:38:13 -0700

    Small refactor

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const mapboxToken = 'pk.eyJ1IjoiYXJuYXYtYWdnYXJ3YWwiLCJhIjoiY2pmbjF4Z3VvMTNsYTJxbmF5YnlwdzJ2OCJ9.E4ecfm2nMeMJQrPODOXR4w';
+const port = 32773;
 const tileLayers = {
 	mapbox: L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + mapboxToken, {
 		attribution:
@@ -10,7 +11,7 @@ const tileLayers = {
 	osmOnline: L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 	}),
-	osmLocal: L.tileLayer('http://localhost:32768/styles/klokantech-basic/{z}/{x}/{y}.png'),
+	osmLocal: L.tileLayer(`http://localhost:${port}/styles/klokantech-basic/{z}/{x}/{y}.png`),
 };
 
 const map = L.map('map').setView([32.7, -117.1], 13);
@@ -21,7 +22,7 @@ const polygon = L.polygon([[32.709, -117.18], [32.703, -117.16], [32.71, -117.14
 const circle = L.circle([32.73, -117.13], {
 	color: 'red',
 	fillColor: '#f03',
-	fillOpacity: 0.5,
+	fillOpacity: 0.4,
 	radius: 1000,
 }).addTo(map);
 
